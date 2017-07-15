05381c84d9338f06887cf3d6c56751b6ba67948f
Nadia Drabyk <nadiadrabyk@Nadias-MacBook-Pro.local>
2017-07-15 13:16:42 +0300

    Initial Commit

---
+++ Onjyb/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Onjyb
+//
+//  Created by Nadia Drabyk on 7/15/17.
+//  Copyright © 2017 Nadia Drabyk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
